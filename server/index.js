4fbdfaaa4d351f53babcdf90af487a22fbe320af
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-29 22:00:24 +0300

    Mongodb connection has been established

--- server/index.js
+++ server/index.js
@@ -11,3 +11,14 @@ app.use(bodyParser.json({limit: "30m", extended: true}))
 // For secure!
 app.use(bodyParser.urlencoded({limit:"30m",extended:true}))
 app.use(cors())
+
+const CONNECTION_URL = "mongodb://localhost:27027/localDatabase"
+const PORT = process.env.PORT || 5000;
+
+// This second options are just for version control to mongodb
+mongoose.connection(CONNECTION_URL, {  useNewUrlParser : true, useUnifiedTopology : true })
+.then(() => console.log(app.listen(PORT, () => console.log(`Server is running at port : ${PORT}`))))
+.catch((error) => console.log(error.message))
+
+// For controlling old version or new version find and update commands!
+mongoose.set('useFindAndModify', false)
