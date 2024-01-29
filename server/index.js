7932e2354dcb4552a0d967087cc49be85c3b0fb7
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-29 22:09:30 +0300

    Added routes file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts',postRoutes)
 
 // 30m restriction, extended for complicated URLs parameters!
 app.use(bodyParser.json({limit: "30m", extended: true}))
@@ -12,13 +15,11 @@ app.use(bodyParser.json({limit: "30m", extended: true}))
 app.use(bodyParser.urlencoded({limit:"30m",extended:true}))
 app.use(cors())
 
-const CONNECTION_URL = "mongodb://localhost:27027/localDatabase"
+const CONNECTION_URL = "mongodb://localhost:27017/localDatabase"
 const PORT = process.env.PORT || 5000;
 
 // This second options are just for version control to mongodb
-mongoose.connection(CONNECTION_URL, {  useNewUrlParser : true, useUnifiedTopology : true })
-.then(() => console.log(app.listen(PORT, () => console.log(`Server is running at port : ${PORT}`))))
+mongoose.connect(CONNECTION_URL)
+.then(() => app.listen(PORT, () => console.log(`Server is running at port : ${PORT}`)))
 .catch((error) => console.log(error.message))
 
-// For controlling old version or new version find and update commands!
-mongoose.set('useFindAndModify', false)
