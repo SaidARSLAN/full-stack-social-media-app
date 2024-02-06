9c590c7ee8dc14e0408143d6a55f78f632b7fe8e
SaidARSLAN <lost.myreligion@hotmail.com>
2024-02-06 22:31:49 +0300

    Added get request

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,18 @@
+import PostMessage from "../models/postMessage.js"
 
 
-export const getPosts = (req,res) => {
+export const getPosts = async (req,res) => {
 
-    res.send('THIS WORKS!')
+    try {
+        const postMessages = await PostMessage.find();
+
+        console.log(postMessages)
+
+        res.status(200).json(postMessages);
+    }
+    catch (err) {
+        res.status(404).json({message : err.message})
+    }
 
 }
 
