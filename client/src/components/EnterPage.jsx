59f2095fcd63981ad67b93cf00fe5755c4ebfa79
SaidARSLAN <lost.myreligion@hotmail.com>
2024-02-11 20:03:47 +0300

    Added login page and form

--- client/src/components/EnterPage.jsx
+++ client/src/components/EnterPage.jsx
@@ -2,25 +2,42 @@ import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import { NavLink } from 'react-router-dom';
-import { enterImage } from '../assets';
+import Form from 'react-bootstrap/Form';
 
 
 
 
 const EnterPage = () => {
-
+  
 
     return (
-            <Container fluid>
-                  <Row>
-                    <Col>
-                        <Image  src={`${enterImage}`} width='100px'/>
+            <Container fluid className='h-100'>
+                  <Row className='h-100'>
+                    <Col className='h-100 main-image'>
                     </Col>
-                    <Col>
-                 <h1>Welcome To MyChat</h1>
-                  <Button variant='primary'><NavLink to='/index' style={{color:'white',textDecoration:'none'}}>Let's dive</NavLink></Button>
+                    <Col xs={8} className='d-flex flex-column justify-content-center'>
+                      <h1 className='text-center mb-4'>Login</h1>
+                      <Form className='d-flex flex-column align-items-center'>
+                      <Form.Group className="mb-3 w-50" controlId="formPlaintextPassword">
+                          <Form.Label className='text-center'>
+                            Email
+                          </Form.Label>
+                          <Col>
+                            <Form.Control type="plaintext" placeholder="Email" />
+                          </Col>
+                        </Form.Group>
+                        <Form.Group className="mb-3 w-50" controlId="formPlaintextPassword">
+                          <Form.Label className='text-center'>
+                            Password
+                          </Form.Label>
+                          <Col>
+                            <Form.Control type="password" placeholder="Password" />
+                          </Col>
+                        </Form.Group>
+                       
+                      <Button variant='primary' className='w-20'><NavLink to='/index' style={{color:'white',textDecoration:'none'}}>Let's dive</NavLink></Button>
+                      </Form>
                     </Col>
 
                   </Row>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
 import {store} from './store/index.js'
-
+import './styles/reset.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
