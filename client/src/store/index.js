bbb372cf5a3af1be1b5629d3f8cd114dbca41ee2
SaidARSLAN <lost.myreligion@hotmail.com>
2024-03-12 10:09:41 +0300

    Added redux

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import user from './user'
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user
+  },
 })
--- client/src/store/user.js
+++ client/src/store/user.js
@@ -1,15 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from 'axios'
+
+
+export const fetchUser = createAsyncThunk('people/fetchUser',
+    async () => {
+        const response = await axios.get('http://localhost:3000/user');
+        console.log(response)
+        return response.data
+    }
+)
+
 
 const initialState = {
     user : {
         username : "",
         email: "",
         password : "",
-    }
+    },
+    status:"idle",
+    error:null
 }
 
 export const userSlice = createSlice({
     name:"counter",
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(fetchUser.pending, (state) => {
+            state.status = "loading"
+        })
+        .addCase(fetchUser.fulfilled, (state,action) => {
+            state.status = "succeeded"
+            state.user = action.payload
+        })
+        .addCase(fetchUser.rejected, (state,action) => {
+            state.status = "failed"
+            state.error = action.error.message
+        })
+    }
 })
+
+export default userSlice.reducer
