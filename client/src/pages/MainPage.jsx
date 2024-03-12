dd711885fc494964df3495d102cd68f4a70943cf
SaidARSLAN <lost.myreligion@hotmail.com>
2024-03-12 17:07:39 +0300

    Added sidebar style

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -2,7 +2,10 @@
 import { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { SET_USER } from '../store/user'
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 
 const MainPage = () => {
 
@@ -14,11 +17,27 @@ const MainPage = () => {
             dispatch(SET_USER(JSON.parse(storedUser)))
         }
     },[dispatch])
-    console.log(user.user)
     return (
-        <div>
-            {user.user.email ? user.user.email : ""}
-        </div>
+        <Container fluid>
+            <Row style={{height:"2000px"}}>
+                <Col>
+                <Nav defaultActiveKey="/home" className="flex-column position-sticky top-0">
+                    <Nav.Link href="/home">Active</Nav.Link>
+                    <Nav.Link eventKey="link-1">Link</Nav.Link>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                    <Nav.Link eventKey="disabled" disabled>
+                        Disabled
+                    </Nav.Link>
+                    </Nav>
+                </Col>
+                <Col>
+                    Middle
+                </Col>
+                <Col>
+                    leftbar
+                </Col>
+            </Row>
+        </Container>
     )
     
 }
